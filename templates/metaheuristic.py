57f83b74034fc4ee8fda3a17c461a5b71a874bfc
jbaginsk <01187206@pw.edu.pl>
2025-12-15 21:28:42 +0100

    implemented two evolution strategies ES(mu + lambda) and ES(mu, lambda)

--- templates/metaheuristic.py
+++ templates/metaheuristic.py
@@ -41,10 +41,11 @@ class Metaheuristic:
             raise Exception("No solution has been found yet.")
 
         # extracting picks and normalizing
-        x = self.x_best.copy()
+        x = self.x_best["decision"].copy()
         picks = np.argsort(x)[-self.k :]
         mask = np.isin(np.arange(self.n), picks)
         x *= mask
+        x[x < 0] = 1e-6  # avoid negative values but have to keep them positive
         normalized = x / x.sum()  # potential division by zero
         normalized = np.nan_to_num(
             normalized, posinf=0.0, neginf=0.0
@@ -69,11 +70,11 @@ class Metaheuristic:
         start_time = time.time()
         while time.time() - start_time <= self.time_deadline:
             # reproduction
-            R = self.tournament_reproduction(curr_popoulation, curr_rate)
+            R = self.reproduction(curr_popoulation, self.children_size)
             # crossover
             C = self.averaging_crossover_extended_version(R)
             # mutation
-            children_popoulation = self.gaussian_mutation(C, self.sigma)
+            children_popoulation = self.mutation(C)
             # evaluation
             children_rate = self.rate(children_popoulation)
             x_t, q_t = self.find_best(children_popoulation, children_rate)
@@ -81,13 +82,34 @@ class Metaheuristic:
                 self.x_best = x_t
                 self.q_best = q_t
             # succession
-            curr_popoulation, curr_rate = self.elite_succession(
-                curr_popoulation, curr_rate, children_popoulation, children_rate
-            )
+            if self.preserve_parents:  # ES(μ + λ) algorithm
+                curr_popoulation, curr_rate = self.elite_succession(
+                    curr_popoulation,
+                    curr_rate,
+                    children_popoulation,
+                    children_rate,
+                    elite_size=self.pop_size,
+                )
+            else:  # ES(μ, λ) algorithm
+                curr_popoulation, curr_rate = self.elite_succession(
+                    children_popoulation,
+                    children_rate,
+                    [],
+                    [],
+                    elite_size=self.pop_size,
+                )
             self.avg_rate_epochs.append(curr_rate.mean())
             self.best_rate_epochs.append(curr_rate.max())
 
-    def __init__(self, time_deadline, problem_path, pop_size=20, sigma=0.5, **kwargs):
+    def __init__(
+        self,
+        time_deadline,
+        problem_path,
+        pop_size=20,
+        children_size=140,
+        preserve_parents=True,
+        **kwargs,
+    ):
         """
         Class initializer. It takes as an argument the maximum computation time (in seconds), controlled externally, and the path that contains the problem instance to be solved
         YOU CAN MODIFY THE HEADER TO INCLUDE OPTIONAL PARAMETERS WITH DEFAULT VALUES ( e.g., __init__(self, time_deadline, problem_path, mut_prob=0.5) )
@@ -104,17 +126,19 @@ class Metaheuristic:
         )
         # TODO: Configure the metaheuristic (e.g., selection operator, crossover, mutation, hyperparameter values, etc.)
         self.pop_size = pop_size
-        self.sigma = sigma  # standard deviation for gaussian mutation
+        self.children_size = children_size
+        self.preserve_parents = preserve_parents
 
     def evaluate(self, x):
         result = 0
-        x = x.copy()  # needed to avoid modifying original array
+        x = x["decision"].copy()  # needed to avoid modifying original array
         # pick k greatest elements from x
         picks = np.argsort(x)[-self.k :]
 
         # changing not chosen elements to zero and normalizing chosen ones
         mask = np.isin(np.arange(self.n), picks)
         x *= mask
+        x[x < 0] = 1e-6  # avoid negative values but have to keep them positive
         normalized = x / x.sum()  # potential division by zero
         normalized = np.nan_to_num(
             normalized, posinf=0.0, neginf=0.0
@@ -136,7 +160,11 @@ class Metaheuristic:
     def initialize_population(self, pop_size):
         population = []
         for _ in range(pop_size):
-            individual = np.random.rand(self.n)
+            individual = {}
+            individual["decision"] = np.random.rand(self.n)
+            individual["sigma"] = (
+                np.random.rand(self.n) * 0.5 + 0.1
+            )  # avoid too small sigma
             population.append(individual)
         return np.array(population)
 
@@ -148,16 +176,11 @@ class Metaheuristic:
         idx = np.argmax(rates)
         return population[idx], rates[idx]
 
-    def tournament_reproduction(self, population, rates):
-        pop_size = len(population)
-        new_population = []
-        for _ in range(pop_size):
-            i, j = np.random.choice(pop_size, size=2)
-            if rates[i] > rates[j]:
-                new_population.append(population[i])
-            else:
-                new_population.append(population[j])
-        return np.array(new_population)
+    def reproduction(self, population, children_size):
+        reproducted = [
+            population[i] for i in np.random.choice(len(population), size=children_size)
+        ]
+        return np.array(reproducted)
 
     def averaging_crossover_extended_version(self, population):
         children = []
@@ -167,30 +190,44 @@ class Metaheuristic:
             parent1 = population[i]
             parent2 = population[j]
             alpha = [np.random.rand() for _ in range(self.n)]
-            child = alpha * parent1 + (1 - np.array(alpha)) * parent2
+            child = {}
+            child["decision"] = (
+                np.array(alpha) * parent1["decision"]
+                + (1 - np.array(alpha)) * parent2["decision"]
+            )
+            child["sigma"] = (
+                np.array(alpha) * parent1["sigma"]
+                + (1 - np.array(alpha)) * parent2["sigma"]
+            )
             children.append(child)
         return np.array(children)
 
-    def gaussian_mutation(self, population, sigma):
+    def mutation(self, population):
+        a = np.random.normal(0, 1)
+        b = np.random.normal(0, 1, size=self.n)
+        tau = 1 / np.sqrt(2 * np.sqrt(self.n))
+        tau_prime = 1 / np.sqrt(2 * self.n)
+
         mutated_population = []
         for individual in population:
-            noise = np.random.normal(0, sigma, size=self.n)
-            mutated_individual = individual + noise
-            mutated_population.append(mutated_individual)
+            new_sigma = individual["sigma"] * np.exp(tau_prime * a + tau * b)
+            new_individual = {}
+            new_individual["decision"] = individual[
+                "decision"
+            ] + new_sigma * np.random.normal(0, 1, size=self.n)
+            new_individual["sigma"] = new_sigma
+            mutated_population.append(new_individual)
         return np.array(mutated_population)
 
     def elite_succession(
-        self, parent_population, parent_rates, child_population, child_rates
+        self, parent_population, parent_rates, child_population, child_rates, elite_size
     ):
-        # hardcoded elite_size = 1, cuz then no need to sort, just pick best and worst
-        survived_parent_idx = np.argmax(parent_rates)
-        survived_parent = parent_population[survived_parent_idx]
-        survived_parent_rate = parent_rates[survived_parent_idx]
-        worst_child_idx = np.argmin(child_rates)
-        if survived_parent_rate > child_rates[worst_child_idx]:
-            child_population[worst_child_idx] = survived_parent
-            child_rates[worst_child_idx] = survived_parent_rate
-        return child_population, child_rates
+        combined_population = np.concatenate((parent_population, child_population))
+        combined_rates = np.concatenate((parent_rates, child_rates))
+        elite_indices = np.argsort(combined_rates)[-elite_size:]
+        new_population = combined_population[elite_indices]
+        new_rates = combined_rates[elite_indices]
+        return new_population, new_rates
 
 
 def draw_graph(v1, v2):
@@ -208,10 +245,19 @@ def draw_graph(v1, v2):
 
 
 if __name__ == "__main__":
+    pop_size = 80
+    chilren_size = pop_size * 7  # typically lambda = 7 * mu
+    print("ES(mu + lambda)")
+    print("Population size:", pop_size, "Children size:", chilren_size)
     met = Metaheuristic(
-        time_deadline=15, problem_path="instances/instance_n100_k10_7.json"
+        time_deadline=60,
+        problem_path="instances/instance_n100_k10_7.json",
+        pop_size=pop_size,
+        children_size=chilren_size,
+        preserve_parents=True,
     )
     met.run()
+    # print("Best solution found:\n", met.x_best)
+    print("Best rate found:", met.q_best)
     print("Best solution found:\n", met.get_best_solution())
-    print("\nBest rate found:", met.q_best)
     draw_graph(met.avg_rate_epochs, met.best_rate_epochs)
